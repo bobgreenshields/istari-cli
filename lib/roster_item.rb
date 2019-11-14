ac89ad3592d8d1c27fde39a301951a10a4ad139c
Bob Greenshields <bob@thegreenshields.com>
2019-11-14 11:32:17 +0000

    Flesh out RosterItem

--- lib/roster_item.rb
+++ lib/roster_item.rb
@@ -2,25 +2,45 @@ class RosterItemError < StandardError
 end
 
 class RosterItem
-	attr_accessor :notes	
-	attr_reader :mob, :mob_id
+	attr_reader :area, :mob, :mob_id, :notes	
+
+	def self.valid_area?(value)
+		return true if value.is_a? Integer
+		return false unless value.is_a? String
+		value.strip.match(/^[+-]?\d+$/)
+	end
+
+	def self.from_hash(item_hash)
+		%w(area mob).each do |key|
+			unless item_hash.has_key?(key)
+				raise RosterItemError, "RosterItem build hash missing a key of #{key}"
+			end
+		end
+		item = self.new
+		item.area = item_hash["area"]
+		item.mob = item_hash["mob"]
+		item.notes = item_hash.fetch("notes", "")
+		item
+	end
 
 	def initialize
 		@area = 0
 	end
 
 	def area=(value)
-		if value.is_a? String
-			value = value.strip
-			unless value.match(/^[+-]?\d+$/)
-				raise RosterItemError, "Area must be an integer or string integer not: #{value}"
-			end
+		unless self.class.valid_area?(value)
+			raise RosterItemError, "Area must be an integer or string integer not: #{value}"
 		end
-			@area = value.to_i
+		@area = value.to_i
 	end
 
-	def area
-		@area.to_s
+	def mob=(value)
+		@mob = value.strip
+		@mob_id = @mob.downcase
+	end
+
+	def notes=(value)
+		@notes = value.strip
 	end
 
 end
--- spec/roster_item_spec.rb
+++ spec/roster_item_spec.rb
@@ -3,10 +3,10 @@ require 'roster_item'
 describe RosterItem do
 	describe '#area' do
 		context 'when set as an integer' do
-			it 'returns a string' do
+			it 'returns a integer' do
 				item = RosterItem.new
 				item.area = 2
-				expect(item.area).to eql("2")
+				expect(item.area).to eql(2)
 			end
 		end
 
@@ -21,8 +21,83 @@ describe RosterItem do
 			it 'ignores the whitespace' do
 				item = RosterItem.new
 				item.area = " 24  "
-				expect(item.area).to eql("24")
+				expect(item.area).to eql(24)
 			end
+		end
+
+		context 'when given a string integer with a plus' do
+			it 'returns a positive integer' do
+				item = RosterItem.new
+				item.area = " +24  "
+				expect(item.area).to eql(24)
+			end
+		end
+
+		context 'when given a string integer with a minus sign' do
+			it 'returns a negative integer' do
+				item = RosterItem.new
+				item.area = " -24  "
+				expect(item.area).to eql(-24)
+			end
+		end
+	end
+
+	describe '#mob' do
+		context 'when mob has leading and trailing whitespace' do
+			it 'trims whitespace' do
+				item = RosterItem.new
+				item.mob = "   Klarg  "
+				expect(item.mob).to eql("Klarg")
+			end
+		end	
+
+		context 'when mob has a capital letters' do
+			it 'mob_id is all lower case' do
+				item = RosterItem.new
+				item.mob = "   KlaRg  "
+				expect(item.mob_id).to eql("klarg")
+			end
+		end
+	end
+
+	describe '.from_hash' do
+		let(:item_hash) { {"area" => "3", "mob" => "Klarg", "notes" => "the leader  "} }
+		it 'creates a RosterItem' do
+			expect(RosterItem.from_hash(item_hash)).to be_a(RosterItem)
+		end
+
+		it 'populates the values' do
+			item = RosterItem.from_hash(item_hash)
+			expect(item.area).to eql(3)
+			expect(item.mob).to eql("Klarg")
+			expect(item.mob_id).to eql("klarg")
+			expect(item.notes).to eql("the leader")
+		end
+
+		context 'when missing an area key' do
+			let(:item_hash) { {"mob" => "Klarg", "notes" => "the leader  "} }
+			it 'raises an error' do
+				expect { RosterItem.from_hash(item_hash) }.to raise_error(RosterItemError)
+			end
+		end
+
+		context 'when missing an mob key' do
+			let(:item_hash) { {"area" => "3", "notes" => "the leader  "} }
+			it 'raises an error' do
+				expect { RosterItem.from_hash(item_hash) }.to raise_error(RosterItemError)
+			end
+		end
+
+		context 'when missing a notes key' do
+			let(:item_hash) { {"area" => "3", "mob" => "Klarg"} }
+			it 'inserts an empty string' do
+				item = RosterItem.from_hash(item_hash)
+				expect(item.area).to eql(3)
+				expect(item.mob).to eql("Klarg")
+				expect(item.mob_id).to eql("klarg")
+				expect(item.notes).to eql("")
+			end
+			
 		end
 	end
 	
