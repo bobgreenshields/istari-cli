8476604e6ef096aed54513d589f25d5583ae1132
Bob Greenshields <bob@thegreenshields.com>
2019-11-14 04:07:18 +0000

    Start adding mobs object

--- lib/mobs.rb
+++ lib/mobs.rb
@@ -1,5 +1,53 @@
+require_relative 'mob'
+
+class MobsError < StandardError
+end
+
 class Mobs
-	def intialize
-		
+
+	def self.from_hash(mobs_hash)
+		mobs = self.new
+		mobs.load_from_hash(mobs_hash)
+		mobs
 	end
+
+	def initialize
+		@mobs_array = []
+		@mobs_hash = {}
+	end
+
+	def load_from_hash(mobs_hash)
+		mobs_hash.each_pair do |key, value|
+			mob = Mob.from_hash(key, value)
+			simple_push(mob)
+		end
+		sort
+	end
+
+	def has_id?(id)
+		id_to_test = id.downcase
+		@mobs_hash.has_key?(id_to_test)
+	end
+
+	def push(mob)
+		if has_id?(mob.id)
+			raise MobsError, "A mob with the id of #{mob.id} already exists"
+		end
+		simple_push(mob)
+		sort
+		self
+	end
+
+	private
+
+	def simple_push(mob)
+		@mobs_array.push(mob)
+		@mobs_hash[mob.id] = mob
+		self
+	end
+
+	def sort
+		@mobs_array = @mobs_array.sort
+	end
+	
 end
--- spec/mobs_spec.rb
+++ spec/mobs_spec.rb
@@ -1,9 +1,41 @@
 require 'mobs'
+require 'mob'
 
 describe Mobs do
 	let(:mob_hash_1) { {"pp" => "9", "loot" => "15sp", "desc" => "a goblin"} }
 	let(:mob_hash_2) { {"pp" => "13", "loot" => "33gp", "desc" => "an owlbear"} }
 	let(:mobs_hash) { {"a" => mob_hash_1, "owlbear" => mob_hash_2} }
+
+	describe '#push' do
+		let(:mob1) { Mob.new("a") }
+		let(:mob2) { Mob.new("b") }
+		let(:mob3) { Mob.new("owlbear") }
+		let(:mobs) { Mobs.new }
+
+		it 'adds a mob object' do
+			mobs.push(mob3).push(mob2).push(mob1)
+			expect(mobs.has_id?("a")).to be_truthy
+		end
+
+		context 'when it already contains a mob with the same id' do
+			# mob ids are always lower case
+			let(:mob4) { Mob.new("B") }
+
+			it 'raises a MobsError' do
+				mobs.push(mob3).push(mob2).push(mob1)
+				expect { mobs.push(mob4) }.to raise_error(MobsError)	
+			end
+		end
+			
+	end
+	
+	
+	describe '.new' do
+		it 'returns a mobs object' do
+			mobs = Mobs.new
+			expect(mobs).to be_a Mobs
+		end
+	end
 
 	describe '.from_hash' do
 		it 'returns a mobs object' do
