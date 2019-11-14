a3381fb482c53af4289a9f290d8f90bc5f0ce682
Bob Greenshields <bob@thegreenshields.com>
2019-11-14 14:46:15 +0000

    Start doing path constants

---
+++ lib/paths.rb
@@ -0,0 +1,13 @@
+require 'pathname'
+
+class Paths
+	attr_reader :istari_root
+	def initialize
+		curr_dir = Pathname.pwd
+		until (curr_dir + "Gemfile").exist?
+			raise StandardError if curr_dir.root?
+			curr_dir = curr_dir.parent
+		end
+		@istari_root = curr_dir
+	end
+end
