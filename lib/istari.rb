e7d524b5e937c0a72fe2b3eff526626922f80863
Bob Greenshields <bob@thegreenshields.com>
2019-11-21 20:03:03 +0000

    Add mobs functionality to Istari

--- lib/istari.rb
+++ lib/istari.rb
@@ -1,4 +1,7 @@
 require_relative 'istari/mob'
+require_relative 'istari/mobs'
+require_relative 'istari/mobs_yaml_loader'
+require_relative 'istari/mobs_table'
 require 'pathname'
 require 'dry/container'
 
@@ -6,6 +9,9 @@ module Istari
 	extend Dry::Container::Mixin
 
 	register(:search_root) { Pathname.pwd }
+	register(:mobs_loader) { MobsYamlLoader.new(self.mobs_file) }
+	register(:table_width) { 120 }
+	register(:mobs_table) { MobsTable.new(self[:table_width]) }
 
 	class << self
 		def istari_root
@@ -21,6 +27,22 @@ module Istari
 				curr_dir = curr_dir.parent
 			end
 			curr_dir
+		end
+
+		def mobs_file
+			istari_root + "_data" + "mobs.yml"
+		end
+
+		def mobs_get
+			self[:mobs_loader].call
+		end
+
+		def mobs_table
+			self[:mobs_table]
+		end
+
+		def mob_create(id)
+			Mob.new(id)
 		end
 		
 	end
--- spec/istari_spec.rb
+++ spec/istari_spec.rb
@@ -1,14 +1,19 @@
 require_relative '../lib/istari'
+require_relative '../lib/istari/mobs'
 require 'dry/container/stub'
 require 'pathname'
 
 Istari.enable_stubs!
 
 describe Istari do
-	it 'registers istari_root' do
-		Istari.stub(:search_root, Pathname.new("spec/fixture").expand_path)
-		expect(Istari[:search_root].to_s).to eql("/home/bobg/dev/istari-cli/spec/fixture")
+	describe '[]:search_root' do
+		it 'registers istari_root' do
+			# Istari.stub(:search_root, Pathname.new("spec/fixture").expand_path)
+			Istari.stub(:search_root, Pathname.new("spec/fixture/_data").expand_path)
+			expect(Istari[:search_root].to_s).to eql("/home/bobg/dev/istari-cli/spec/fixture/_data")
+		end
 	end
+
 	describe '.find_istari_root' do
 		it 'returns a Pathname' do
 			expect(Istari.find_istari_root).to be_a Pathname
@@ -23,6 +28,20 @@ describe Istari do
 				expect { Istari.find_istari_root }.to raise_error(StandardError)
 			end
 		end
-		
 	end
+
+	describe '.mobs_file' do
+		it 'returns the location of mobs.yml' do
+			Istari.stub(:search_root, Pathname.new("spec/fixture/_data").expand_path)
+			expect(Istari.mobs_file).to be_a Pathname
+			expect(Istari.mobs_file.to_s).to eql("/home/bobg/dev/istari-cli/spec/fixture/_data/mobs.yml")
+		end
+	end
+
+	describe '.mobs_get' do
+		it 'retunds a populated mobs object' do
+			expect(Istari.mobs_get).to be_a Istari::Mobs
+		end
+	end
+
 end
