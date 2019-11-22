519445d670ff57e6070d6518b529856fdac3cddc
Bob Greenshields <bob@thegreenshields.com>
2019-11-22 09:09:07 +0000

    Get mob data as input

--- lib/istari/cli/mobs.rb
+++ lib/istari/cli/mobs.rb
@@ -6,12 +6,33 @@ module Istari
 		class Mobs < Thor
 			desc "add", "add a new mob"
 			def add
-				puts "adding a mob"
+				puts "The next available generic id is: #{mobs.next_id}"
+				puts "To use generic id simply press return"
+				puts ""
+				id = ask("Enter id (. to exit) id?").strip.downcase
+				id = mobs.next_id if id == ""
+				exit if id == "."
+				while mobs.has_id?(id)
+					puts set_color("This id: #{id} is already in use please enter a different one", :red)
+					id = ask("Enter id (. to exit) id?").strip.downcase
+					exit if id == "."
+				end
+				puts set_color("Using id: #{id}", :green)
 			end
 
 			desc "list", "list all current mobs"
 			def list
-				puts Istari.mobs_table.call(Istari.mobs_get)
+				puts Istari.mobs_table.call(mobs)
+			end
+
+			private
+			
+			def mobs
+				@mobs || refresh_mobs
+			end
+
+			def refresh_mobs
+				@mobs = Istari.mobs_get
 			end
 		end
 		
