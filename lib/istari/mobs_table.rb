b3b65d6f25b294a2394ed99e4135f79d2fe60cbc
Bob Greenshields <bob@thegreenshields.com>
2019-11-22 19:46:07 +0000

    Move AreasTable into Module Istari

--- lib/istari/mobs_table.rb
+++ lib/istari/mobs_table.rb
@@ -1,13 +1,14 @@
 require_relative 'mobs'
 require 'tabulo'
 
-class MobsTable
-	def initialize(table_width)
-		@table_width = table_width
-	end
+module Istari
+	class MobsTable
+		def initialize(table_width)
+			@table_width = table_width
+		end
 
-	def call(mobs)
-		Tabulo::Table.new(mobs.each, :id, :desc, :pp, :loot).pack(max_table_width: @table_width).to_s
+		def call(mobs)
+			Tabulo::Table.new(mobs.each, :id, :desc, :pp, :loot).pack(max_table_width: @table_width).to_s
+		end
 	end
 end
-
