83265dc56c0a0684e2636889377851e591e2f741
Bob Greenshields <bob@thegreenshields.com>
2019-11-25 11:22:00 +0000

    Add areas functionality to Istari module

--- lib/istari.rb
+++ lib/istari.rb
@@ -2,6 +2,8 @@ require_relative 'istari/mob'
 require_relative 'istari/mobs'
 require_relative 'istari/mobs_yaml_loader'
 require_relative 'istari/mobs_table'
+require_relative 'istari/areas_yaml_loader'
+require_relative 'istari/areas_table'
 require 'pathname'
 require 'dry/container'
 
@@ -9,10 +11,12 @@ module Istari
 	extend Dry::Container::Mixin
 
 	register(:search_root) { Pathname.pwd }
-	register(:mobs_loader) { MobsYamlLoader.new(self.mobs_file) }
 	register(:table_width) { 120 }
-	register(:mobs_table) { MobsTable.new(self[:table_width]) }
 	register(:backup_dir_name) { ".backups" }
+	register(:mobs_loader) { MobsYamlLoader.new(self.mobs_file) }
+	register(:mobs_table) { MobsTable.new(self[:table_width]) }
+	register(:areas_loader) { AreasYamlLoader.new(self.areas_dir) }
+	register(:areas_table) { AreasTable.new(self[:table_width]) }
 
 	class << self
 		def istari_root
@@ -34,12 +38,24 @@ module Istari
 			istari_root + "_data" + "mobs.yml"
 		end
 
+		def areas_dir
+			istari_root + "_areas"
+		end
+
 		def mobs_get
 			self[:mobs_loader].call
 		end
 
 		def mobs_table
 			self[:mobs_table]
+		end
+
+		def areas_get
+			self[:areas_loader].call
+		end
+
+		def areas_table
+			self[:areas_table]
 		end
 
 		def mob_create(id)
