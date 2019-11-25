b9bd1cadd68936f774dea8e3fa04fd554ac83f95
Bob Greenshields <bob@thegreenshields.com>
2019-11-25 12:48:08 +0000

    Add roster functionality to Istari module

--- lib/istari.rb
+++ lib/istari.rb
@@ -4,6 +4,8 @@ require_relative 'istari/mobs_yaml_loader'
 require_relative 'istari/mobs_table'
 require_relative 'istari/areas_yaml_loader'
 require_relative 'istari/areas_table'
+require_relative 'istari/roster_yaml_loader'
+require_relative 'istari/roster_table'
 require 'pathname'
 require 'dry/container'
 
@@ -17,6 +19,8 @@ module Istari
 	register(:mobs_table) { MobsTable.new(self[:table_width]) }
 	register(:areas_loader) { AreasYamlLoader.new(self.areas_dir) }
 	register(:areas_table) { AreasTable.new(self[:table_width]) }
+	register(:roster_loader) { RosterYamlLoader.new(self.roster_file) }
+	register(:roster_table) { RosterTable.new(self[:table_width]) }
 
 	class << self
 		def istari_root
@@ -42,6 +46,10 @@ module Istari
 			istari_root + "_areas"
 		end
 
+		def roster_file
+			istari_root + "_data" + "01-default.yml"
+		end
+
 		def mobs_get
 			self[:mobs_loader].call
 		end
@@ -56,6 +64,14 @@ module Istari
 
 		def areas_table
 			self[:areas_table]
+		end
+
+		def roster_get
+			self[:roster_loader].call
+		end
+
+		def roster_table
+			self[:roster_table]
 		end
 
 		def mob_create(id)
