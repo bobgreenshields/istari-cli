0e7979b33cd6c26cb4cdcabfc941a5f4bedee0c2
Bob Greenshields <bob@thegreenshields.com>
2019-11-25 11:22:23 +0000

    Add cli areas list

--- lib/istari/cli.rb
+++ lib/istari/cli.rb
@@ -1,11 +1,15 @@
 require 'thor'
 require_relative './cli/mobs'
+require_relative './cli/areas'
 
 module Istari
 	module Cli
 		class App < Thor
 			desc "mobs SUBCOMMAND ...ARGS", "create and manage the encounter's mobs"	
 			subcommand "mobs", Istari::Cli::Mobs
+
+			desc "areas SUBCOMMAND ...ARGS", "create and manage the encounter's areas"	
+			subcommand "areas", Istari::Cli::Areas
 		end
 	end
 	
